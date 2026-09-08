8fa911771d6d6b00cf602be5410da20da33008f9
filbertrei <filbertrei12@gmail.com>
2026-09-08 16:57:30 +0700

    Fix vercel api function directory and routes

--- laravel/api/index.php
+++ laravel/api/index.php
@@ -1,3 +1,11 @@
 <?php
-// Forward Vercel requests ke index.php publik Laravel
-require __DIR__ . '/../public/index.php';
+// Forward request to public files
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
+    return false;
+}
+
+require_once __DIR__ . '/../public/index.php';
